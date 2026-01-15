08c54182b628ff689e6323d942eaea3061610f5c
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-01-15 20:35:29 +0700

    add some hope of tailwind sorting, but seems like not going to work

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import { invoke, isTauri } from "@tauri-apps/api/core";
   import { Loader2Icon } from "@lucide/svelte";
   import { fade } from "svelte/transition";
-    import { getAudioContext, initCustomModules } from "$lib/audiocontext";
+  import { getAudioContext, initCustomModules } from "$lib/audiocontext";
 
   let perm: Promise<any>;
   const tauri = isTauri();
@@ -24,10 +24,7 @@
   }
 
   const context = getAudioContext();
-  const p = Promise.all([
-    initCustomModules(),
-    perm,
-  ]);
+  const p = Promise.all([initCustomModules(), perm]);
 
   const loaded = p.then(() => {
     return gitGud(context, tauri);
