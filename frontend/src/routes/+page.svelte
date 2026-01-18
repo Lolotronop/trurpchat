eaea042878e5d37a94771bbef26b3b4fa5017ea6
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-01-19 01:39:24 +0700

    a bit of a servers resturucture

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import * as Dialog from "$lib/components/ui/dialog";
-  import * as Avatar from "$lib/components/ui/avatar";
-  import * as Tooltip from "$lib/components/ui/tooltip";
-  import { Button } from "$lib/components/ui/button";
   import { gitGud } from "$lib/god.svelte";
   import BottomControls from "./BottomControls.svelte";
   import RoomList from "./rooms/RoomList.svelte";
   import Stream from "./Stream.svelte";
-  import ServerForm from "./ServerForm.svelte";
   import Users from "./Users.svelte";
+  import Servers from "./servers/Servers.svelte";
   const g = gitGud();
   // TODO: this needs to be removed with proper
   // "speaking" sending
@@ -17,61 +13,12 @@
 
 <main class="flex h-screen w-screen">
   <div class="flex h-full flex-col border-r p-2">
-    <div class="flex shrink flex-col items-center gap-1">
-      {#each g.servers.values as server (server.definition.url)}
-        <Tooltip.Provider>
-          <Tooltip.Root delayDuration={100}>
-            <Tooltip.Trigger class="max-w-28">
-              <Avatar.Root
-                class="size-10"
-                onclick={() => {
-                g.servers.selected = server;
-              }}
-              >
-                <!-- <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" /> -->
-                <Avatar.Fallback class="select-none">
-                  {server.definition.name[0].toUpperCase()}
-                </Avatar.Fallback>
-              </Avatar.Root>
-            </Tooltip.Trigger>
-            <Tooltip.Content>
-              {server.definition.name}
-              <Button
-                onclick={() => {
-                g.servers.remove(server);
-              }}
-              >
-                Remove
-              </Button>
-            </Tooltip.Content>
-          </Tooltip.Root>
-        </Tooltip.Provider>
-      {/each}
-    </div>
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <Button variant="ghost" class="size-8">
-          <div class="flex flex-row items-center gap-2">+</div>
-        </Button>
-      </Dialog.Trigger>
-      <Dialog.Content class="max-w-2xl p-0! px-0! py-0!">
-        <ServerForm />
-      </Dialog.Content>
-    </Dialog.Root>
+    <Servers servers={g.servers} />
   </div>
   <div class="flex h-full min-w-75 flex-col border-r">
     <div class="flex w-full p-2 px-6 text-xl">
       {g.servers.selected?.definition.name || "Select a server"}
     </div>
-    <Button
-      onclick={() => {
-      g.servers.selected?.reconnect();
-    }}
-      variant="ghost"
-      class="w-full p-2"
-    >
-      Reconect
-    </Button>
     <RoomList />
     <div class="w-full p-0.5">
       <BottomControls />
---
+++ frontend/src/routes/servers/Servers.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
+  import * as Tooltip from "$lib/components/ui/tooltip";
+  import * as Dialog from "$lib/components/ui/dialog";
+  import { Button } from "$lib/components/ui/button";
+  import type { ServerManager } from "$lib/servers.svelte";
+  import ServerForm from "./ServerForm.svelte";
+
+  type Props = {
+    servers: ServerManager;
+  };
+
+  const { servers }: Props = $props();
+</script>
+<div class="flex shrink flex-col items-center gap-1">
+  {#each servers.values as server (server.definition.url)}
+    <Tooltip.Provider>
+      <Tooltip.Root delayDuration={100}>
+        <Tooltip.Trigger class="max-w-28">
+          <Avatar.Root
+            class="size-10"
+            onclick={() => {
+                servers.selected = server;
+              }}
+          >
+            <!-- <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" /> -->
+            <Avatar.Fallback class="select-none">
+              {server.definition.name[0].toUpperCase()}
+            </Avatar.Fallback>
+          </Avatar.Root>
+        </Tooltip.Trigger>
+        <Tooltip.Content>
+          {server.definition.name}
+          <Button
+            onclick={() => {
+                servers.remove(server);
+              }}
+          >
+            Remove
+          </Button>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  {/each}
+
+  <Dialog.Root>
+    <Dialog.Trigger>
+      <Button variant="ghost" class="size-8">
+        <div class="flex flex-row items-center gap-2">+</div>
+      </Button>
+    </Dialog.Trigger>
+    <Dialog.Content class="max-w-2xl p-0! px-0! py-0!">
+      <ServerForm />
+    </Dialog.Content>
+  </Dialog.Root>
+</div>
